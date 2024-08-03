3f4f6994a5498dc0b442e0f8ab6ffb57de49af83
danieltikamori <tikamori.itiro@gmail.com>
2024-08-04 06:20:46 +0900

    Add JWT authentication configuration

---
+++ src/main/java/me/amlu/config/AppConfig.java
@@ -0,0 +1,61 @@
+package me.amlu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(management -> management.sessionCreationPolicy(
+            SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        Authorize -> Authorize
+                                .requestMatchers("/api/v1/admin/**")
+                                .hasAnyRole("ADMIN", "RESTAURANT_OWNER")
+                                .requestMatchers("/api/**").authenticated()
+                                .anyRequest().permitAll()
+                ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        return null;
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration corsConfiguration = new CorsConfiguration();
+            corsConfiguration.applyPermitDefaultValues();
+            corsConfiguration.setAllowedOrigins(Arrays.asList(
+                    "https://tika-food.vercel.app/",
+                    "http://localhost:3000"
+            ));
+            corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+            corsConfiguration.setAllowCredentials(true);
+            corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+            corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
+            corsConfiguration.setMaxAge(3600L);
+
+            return corsConfiguration;
+        };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/me/amlu/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package me.amlu.config;
+
+public class JwtConstant {
+
+    public static final String JWT_SECRET = "FillWithMyRandomSecretKey";
+    public static final String JWT_HEADER = "Authorization";
+    public static final String JWT_PREFIX = "Bearer";
+}
