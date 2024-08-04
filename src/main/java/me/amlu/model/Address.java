8f1835a8dda7241cc2299c146b4710551419170f
danieltikamori <tikamori.itiro@gmail.com>
2024-08-04 13:20:53 +0900

    Fix: simplify the address data

--- src/main/java/me/amlu/model/Address.java
+++ src/main/java/me/amlu/model/Address.java
@@ -11,10 +11,5 @@ public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String addressLine1;
-    private String addressLine2;
-    private String city;
-    private String state;
-    private String country;
-    private String postalCode;
+    private String address;
 }
