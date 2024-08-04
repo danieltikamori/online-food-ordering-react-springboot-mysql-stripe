623a207d2f69bcbd2e7a12472675ffa4d1957108
danieltikamori <tikamori.itiro@gmail.com>
2024-08-04 15:11:44 +0900

    Add Address Repository

--- src/main/java/me/amlu/model/Address.java
+++ src/main/java/me/amlu/model/Address.java
@@ -4,12 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Setter;
 
 @Entity
+@Setter
 public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String address;
+
 }
---
+++ src/main/java/me/amlu/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package me.amlu.repository;
+
+import me.amlu.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
