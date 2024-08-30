f6f06e31cb22524afaa868c4c7c81aa6be5e8134
danieltikamori <tikamori.itiro@gmail.com>
2024-08-30 20:45:21 +0900

    Fix: some browsers send token as object

--- src/main/java/me/amlu/controller/CartController.java
+++ src/main/java/me/amlu/controller/CartController.java
@@ -7,6 +7,8 @@ import me.amlu.request.AddCartItemRequest;
 import me.amlu.request.UpdateCartItemRequest;
 import me.amlu.service.CartService;
 import me.amlu.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +16,15 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 public class CartController {
 
     private final CartService cartService;
 
     private final UserService userService;
+
+    private final Logger log = LoggerFactory.getLogger(CartController.class);
+
 
     public CartController(CartService cartService, UserService userService) {
         this.cartService = cartService;
@@ -30,13 +35,21 @@ public class CartController {
     @PutMapping("/cart/add")
     public ResponseEntity<CartItem> addItemToCart(@Valid @RequestBody AddCartItemRequest request,
                                            @RequestHeader("Authorization") String token) throws Exception {
+        // 1. Extract JWT token from Authorization header
+//        String token = authHeader.substring("Bearer ".length());
+
+        // 2. Validate JWT token and add item to cart
+//        log.info("Received token: {}", token);
         CartItem cartItem = cartService.addItemToCart(request, token);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(cartItem);
+        // 3. Return the cart item
+        return ResponseEntity.status(HttpStatus.OK).body(cartItem);
+
+
 //        return new ResponseEntity<>(cartItem, HttpStatus.OK);
     }
 
-    @PutMapping("/cart/update")
+    @PutMapping("/cart-item/update")
     public ResponseEntity<CartItem> updateCartItemQuantity(@Valid @RequestBody UpdateCartItemRequest request,
                                                            @RequestHeader("Authorization") String token) throws Exception {
         CartItem cartItem = cartService.updateCartItemQuantity(request.getCartItemId(), request.getQuantity());
@@ -52,7 +65,7 @@ public class CartController {
         return ResponseEntity.status(HttpStatus.OK).body(cartItem);
     }
 
-    @PutMapping("cart/clear")
+    @PutMapping("/cart/clear")
     public ResponseEntity<Cart> clearCart(@RequestHeader("Authorization") String token) throws Exception {
 
         User user = userService.findUserByJwtToken(token);
